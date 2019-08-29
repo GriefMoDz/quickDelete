e435279540b038b653ac12323f2b1765da37a30d
Harley <GriefMoDz@gmail.com>
2019-08-29 13:05:17 +1000

    [v1.1.0] Various fixes and enhancements

--- index.js
+++ index.js
@@ -1,66 +1,47 @@
 const { Plugin } = require('powercord/entities');
-const { getOwnerInstance, waitFor } = require('powercord/util');
-const { contextMenu, getModule } = require('powercord/webpack');
+const { forceUpdateElement, getOwnerInstance, waitFor } = require('powercord/util');
+const { getModule, messages, constants: { Permissions } } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 
 module.exports = class QuickDelete extends Plugin {
+  async startPlugin () {
+    this.messageClasses = (await getModule([ 'messageCompact', 'messageCozy' ]));
+    this.messageQuery = `.${this.messageClasses.message.replace(/ /g, '.')}`;
+    this.patchMessageContent();
+  }
 
-    async startPlugin() {
-        this.patchMessageContent();
+  pluginWillUnload () {
+    uninject('pc-quickDelete-MessageContent');
+    forceUpdateElement(this.messageQuery, true);
+  }
+
+  async patchMessageContent () {
+    const _this = this;
+    const instance = getOwnerInstance(await waitFor(this.messageQuery));
+
+    const permissions = (await getModule([ 'getChannelPermissions' ]));
+    const currentUser = (await getModule([ 'getCurrentUser' ])).getCurrentUser();
+
+    function renderMessage (_, res) {
+      const { message, channel } = this.props;
+      const hasDeletePermission = permissions.can(Permissions.MANAGE_MESSAGES, channel);
+
+      if (hasDeletePermission || message.author.id === currentUser.id) {
+        res.props.onClick = _this.handleMessageDelete(channel.id, message.id);
+      }
+
+      return res;
     }
 
-    pluginWillUnload() {
-        uninject('pc-quickDelete-MessageContent');
-    }
+    inject('pc-quickDelete-MessageContent', instance.__proto__, 'render', renderMessage);
+    forceUpdateElement(this.messageQuery, true);
+  }
 
-    async patchMessageContent() {
-        const _this = this;
-
-        const messageClasses = await getModule(['messageCompact', 'messageCozy']);
-        const permissions = await require('powercord/webpack').getModule(['getChannelPermissions'])
-        const messageQuery = `.${messageClasses.message.replace(/ /g, '.')}`;
-
-        const instance = getOwnerInstance(await waitFor(messageQuery));
-        const currentUser = (await getModule(['getCurrentUser'])).getCurrentUser();
-
-
-        function renderMessage(_, res) {
-            const { message, channel } = this.props;
-
-            const deletePermission = permissions.can(require('powercord/webpack').constants.Permissions.MANAGE_MESSAGES, channel);
-
-            if (deletePermission || message.author.id === currentUser.id) {
-                res.props.onMouseUp = _this.handleMessageDelete(channel.id, message.id);
-            }
-
-            return res;
-        }
-
-        inject('pc-quickDelete-MessageContent', instance.__proto__, 'render', renderMessage);
-
-        this.forceUpdate(messageQuery);
-    }
-
-    handleMessageDelete(channelId, messageId) {
-        return async (e) => {
-            if (e.ctrlKey == true) {
-                require('powercord/webpack').messages.deleteMessage(channelId, messageId, false)
-            }
-        }
-    }
-
-    /*
-    * DISCLAIMER: the following method was taken from .intrnl#6380's 'blackboxTags'
-    * plug-in - this section of code does not belong to me.
-    */
-    forceUpdate(query) {
-        const elements = [
-            ...document.querySelectorAll(query)
-        ];
-
-        for (const elem of elements) {
-            const instance = getOwnerInstance(elem);
-            instance.forceUpdate();
-        }
-    }
-}
+  handleMessageDelete (channelId, messageId) {
+    return (e) => {
+      if (e.ctrlKey) {
+        messages.deleteMessage(channelId, messageId, false);
+      }
+    };
+  }
+};
