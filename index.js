e32a3fd07dd1193d4e042d14ef763b99762cf09a
Harley <GriefMoDz@gmail.com>
2019-11-16 18:40:33 +1100

    [v1.2.0] Additional functionality and small fixes
    * Holding down the 'Ctrl' key alone will now display a confirmation modal, but can be skipped if the 'Shift' key is also held down.
    * Message deletions can now be staged with the 'Delete' key which will in turn skip the forementioned confirmation modal.
    * Provide an option in the confirmation modal to report the selected message to Discord's Trust & Safety Team before deleting a message.
    * Bump version number from '1.1.0' to '1.2.0'.

--- index.js
+++ index.js
@@ -5,21 +5,38 @@ const { inject, uninject } = require('powercord/injector');
 
 module.exports = class QuickDelete extends Plugin {
   async startPlugin () {
-    this.messageClasses = (await getModule([ 'container', 'messageCompact' ]));
-    this.messageQuery = `.${this.messageClasses.content.replace(/ /g, '.')}`;
+    this.messageClasses = await getModule([ 'container', 'messageCompact' ]);
+    this.messageQuery = `.${this.messageClasses.container.split(' ')[0]} > div`;
+    this.patchReportMessage();
     this.patchMessageContent();
   }
 
   pluginWillUnload () {
-    uninject('pc-quickDelete-MessageContent');
+    const report = getModule([ 'canReportInChannel' ], false);
+    report.canReportMessage = report.__canReportMessage;
+
+    uninject('quickDelete-MessageContent');
     forceUpdateElement(this.messageQuery, true);
+  }
+
+  async patchReportMessage () {
+    const report = await getModule([ 'canReportInChannel' ]);
+    const id = (await getModule([ 'getId' ])).getId();
+
+    report.canReportMessage = (canReportMessage => (message) => {
+      if (message && message.author.id !== id) {
+        return true;
+      }
+
+      return canReportMessage(message);
+    })(report.__canReportMessage = report.canReportMessage);
   }
 
   async patchMessageContent () {
     const _this = this;
     const instance = getOwnerInstance(await waitFor(this.messageQuery));
 
-    const permissions = (await getModule([ 'getChannelPermissions' ]));
+    const permissions = await getModule([ 'getChannelPermissions' ]);
     const currentUser = (await getModule([ 'getCurrentUser' ])).getCurrentUser();
 
     function renderMessage (_, res) {
@@ -27,20 +44,46 @@ module.exports = class QuickDelete extends Plugin {
       const hasDeletePermission = permissions.can(Permissions.MANAGE_MESSAGES, channel);
 
       if (hasDeletePermission || message.author.id === currentUser.id) {
-        res.props.onClick = _this.handleMessageDelete(channel.id, message.id);
+        res.props.onClick = _this.handleMessageDelete(channel, message);
       }
 
       return res;
     }
 
-    inject('pc-quickDelete-MessageContent', instance.__proto__, 'render', renderMessage);
+    inject('quickDelete-MessageContent', instance.__proto__, 'render', renderMessage);
     forceUpdateElement(this.messageQuery, true);
   }
 
-  handleMessageDelete (channelId, messageId) {
+  handleMessageDelete (channel, message) {
+    let isHoldingDelete = false;
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Delete') {
+        isHoldingDelete = true;
+      }
+    });
+
+    document.addEventListener('keyup', (e) => {
+      if (e.key === 'Delete') {
+        isHoldingDelete = false;
+      }
+    });
+
+    messages.confirmDelete = getModule([ 'confirmDelete' ], false).confirmDelete;
+
     return (e) => {
-      if (e.ctrlKey) {
-        messages.deleteMessage(channelId, messageId, false);
+      if ((e.shiftKey && e.ctrlKey) || isHoldingDelete) {
+        messages.deleteMessage(channel.id, message.id, false);
+      } else if (e.ctrlKey) {
+        messages.confirmDelete(channel, message, true);
+
+        const tipClass = `.${getModule([ 'inline', 'tip' ], false).tip.split(' ')[0]}`;
+
+        setTimeout(() => {
+          const tip = document.querySelector(tipClass);
+          if (tip) {
+            tip.innerHTML = 'You can hold down shift before clicking a message that you want to delete to bypass this confirmation entirely.';
+          }
+        }, 100);
       }
     };
   }
